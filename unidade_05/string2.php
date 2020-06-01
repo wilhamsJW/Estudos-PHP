e3eecac05579b3f451009bcdf33191f0f3b9f23d
wilhamsJW <wilhamsmeira@gmail.com>
2020-06-01 20:37:05 -0300

    strings, variáveis

--- unidade_05/string2.php
+++ unidade_05/string2.php
@@ -12,25 +12,29 @@
     <body>
         
         <?php 
-            // strlen - Retorna primeira ocorrencia
+            // strlen - Retorna primeira ocorrencia, retornou a quantidade de caractere da variável
+            echo strlen($_nome) . "</br>";
             
-            
-            // stripos  - Retorna primeira ocorrência 
-            
+            // stripos  - Retorna primeira ocorrência - me retorna a posição da var que é 1 0 é a letra c
+            echo stripos($_nome, 'u') . "</br>"; 
 
-            // strripos - Retorna última ocorrência
-            
+            // strripos - Retorna última ocorrência - retorna a última posição que ele encontrou a letr u
+            echo strripos($_nome, 'u') . "</br>"; 
+
             
             // strtolower - converte para letras min.
-            
+            echo strtolower($_nome) . "</br>"; 
 
-            // strtoupper - converte para letras min.
-            
-
+            // strtoupper - converte para letras maisc. Todas as letras aparecerão maiusculas
+           
+        
             // SUBSTR_COUNT - Conta quantas ocorreram
             // de um texto ou string
             // Faz diferença Maiusculas e minusculas
-            
+            //conta quantas vezes aparece o a
+            //ele pesquisa palavra tbm
+            //php.net é o site com a documentação da página
+            echo SUBSTR_COUNT($_nome, 'a') . "</br>"; 
         ?>
         
         
